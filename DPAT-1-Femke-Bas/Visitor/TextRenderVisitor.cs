e39cc6cc68becbfbbbdd1ef0f3b31ed3d34447de
fvanhastenberg <f.vanhastenberg@student.avans.nl>
2026-06-16 18:07:07 +0200

    feat: visitor pattern added

--- DPAT-1-Femke-Bas/Visitor/TextRenderVisitor.cs
+++ DPAT-1-Femke-Bas/Visitor/TextRenderVisitor.cs
@@ -1,13 +1,54 @@
 using Domain;
+using System.Text;
 
 namespace DPAT_1_Femke_Bas.Visitor;
 
 public class TextRenderVisitor : IFsmVisitor
 {
-    public void Visit(StateMachine stateMachine) { }
-    public void Visit(CompoundState state) { }
-    public void Visit(SimpleState state) { }
-    public void Visit(InitialState state) { }
-    public void Visit(FinalState state) { }
-    public void Visit(Transition transition) { }
+    private readonly StringBuilder _stringBuilder = new StringBuilder();
+
+    public void Visit(StateMachine stateMachine) 
+    { 
+        _stringBuilder.AppendLine("Rendering StateMachine:");
+        
+        foreach (var state in stateMachine.RootStates)
+        {
+            state.Accept(this);
+        }
+    }
+    
+    public void Visit(CompoundState state) 
+    { 
+        _stringBuilder.AppendLine("- CompoundState");
+
+        foreach (var childState in state.Children) 
+        {
+            childState.Accept(this);
+        }
+    }
+    
+    public void Visit(SimpleState state) 
+    { 
+        _stringBuilder.AppendLine("- SimpleState");
+    }
+    
+    public void Visit(InitialState state) 
+    { 
+        _stringBuilder.AppendLine("- InitialState");
+    }
+    
+    public void Visit(FinalState state) 
+    { 
+        _stringBuilder.AppendLine("- FinalState");
+    }
+    
+    public void Visit(Transition transition) 
+    { 
+        _stringBuilder.AppendLine("  -> Transition");
+    }
+
+    public string GetRenderedText()
+    {
+        return _stringBuilder.ToString();
+    }
 }
